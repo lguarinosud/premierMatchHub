a4e189ac241124839fbc2485b1da50d6a6275695
Leonardo Guarino <leonardo.guarino@redcentricplc.com>
2024-07-19 01:48:17 +0100

    Week 1 completed. Format amended

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,19 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: "src/",
+    root: "src/",
 
-  build: {
-    outDir: "../dist",
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "src/index.html"),
-        cart: resolve(__dirname, "src/cart/index.html"),
-        product: resolve(__dirname, "src/product_pages/index.html"),
-        checkout: resolve(__dirname, "src/checkout/index.html"),
-        success: resolve(__dirname, "src/checkout/success.html"),
-        product_listing: resolve(__dirname, "src/product-listing/index.html"),
-      },
+    build: {
+        outDir: "../dist",
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "src/index.html"),
+                //cart: resolve(__dirname, "src/cart/index.html"),
+                //product: resolve(__dirname, "src/product_pages/index.html"),
+                //checkout: resolve(__dirname, "src/checkout/index.html"),
+                //success: resolve(__dirname, "src/checkout/success.html"),
+                //product_listing: resolve(__dirname, "src/product-listing/index.html"),
+            },
+        },
     },
-  },
 });
